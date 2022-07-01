1ffdc046767b6cca11af70f337397f7ceea5d5b2
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-07-01 21:40:36 +0300

    solved task

--- programmingFundamentalsJsMay2022/dataTypesAndVariablesLab/echoType.js
+++ programmingFundamentalsJsMay2022/dataTypesAndVariablesLab/echoType.js
@@ -1,3 +1,12 @@
-function echoType(params) {
-    
+function echoType(param) {
+    let typeOf = typeof param;
+    console.log(typeOf);
+    if (typeOf === 'string' || typeOf === 'number') {
+        console.log(param);
+    } else {
+        console.log('Parameter is not suitable for printing');
+    }
 }
+echoType(18);
+echoType('Hello, JavaScript!');
+echoType(null);
