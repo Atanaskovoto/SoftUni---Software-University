6b762a9b912aed701a163bb9ccf3049ae0ece3af
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-07-12 23:48:15 +0300

    format tasks

--- programmingFundamentalsJsMay2022/ArraysLab/reverseInPlace.js
+++ programmingFundamentalsJsMay2022/ArraysLab/reverseInPlace.js
@@ -1,9 +1,8 @@
 function reverseInPlace(array) {
-    
     let printLine = '';
     for (let i = array.length - 1; i >= 0; i--) {
         printLine += `${array[i]} `;
     }
-        console.log(printLine);
+    console.log(printLine);
 }
 reverseInPlace(['a', 'b', 'c', 'd', 'e']);
