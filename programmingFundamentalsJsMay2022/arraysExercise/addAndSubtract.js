25d90e127724ae5c534a4ec19c03c37e92367bc3
Atanas Apostolov <103858849+Atanaskovoto@users.noreply.github.com>
2022-07-20 12:31:08 +0300

    Optimization of addAndSubtract task

--- programmingFundamentalsJsMay2022/arraysExercise/addAndSubtract.js
+++ programmingFundamentalsJsMay2022/arraysExercise/addAndSubtract.js
@@ -1,22 +1,18 @@
 function addAndSubtract(array) {
-    let newArray = [];
+    let newArray = [],
+        arraySum = 0,
+        newArraySum = 0;
     for (let i = 0; i < array.length; i++) {
+        arraySum += array[i];
         if (array[i] % 2 === 0) {
             newArray[i] = array[i] + i;
         } else {
             newArray[i] = array[i] - i;
         }
+        newArraySum += newArray[i];
     }
     console.log(newArray);
-    let arraySum = 0,
-        newArraySum = 0;
-    for (let el of array) {
-        arraySum += el;
-    }
     console.log(arraySum);
-    for (let el of newArray) {
-        newArraySum += el;
-    }
     console.log(newArraySum);
 }
 addAndSubtract([5, 15, 23, 56, 35])
