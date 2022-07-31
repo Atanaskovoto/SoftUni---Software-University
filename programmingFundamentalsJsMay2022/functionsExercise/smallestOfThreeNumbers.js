18b0cfd82ecd7366f470ba07befc5b131a0147b0
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-07-31 15:39:44 +0300

    Update smallestOfThreeNumbers.js

--- programmingFundamentalsJsMay2022/functionsExercise/smallestOfThreeNumbers.js
+++ programmingFundamentalsJsMay2022/functionsExercise/smallestOfThreeNumbers.js
@@ -1,11 +1,12 @@
 function smallestOfThreeNumbers(numOne, numTwo, numThree) {
-    let smallestNumber = numOne;
-    if (smallestNumber > numTwo) {
-        smallestNumber = numTwo
-    }
-    if (smallestNumber > numThree) {
-        smallestNumber = numThree
-    }
+    // let smallestNumber = numOne;
+    // if (smallestNumber > numTwo) {
+    //     smallestNumber = numTwo
+    // }
+    // if (smallestNumber > numThree) {
+    //     smallestNumber = numThree
+    // }
+    let smallestNumber = Math.min(numOne, numTwo, numThree)
     console.log(smallestNumber);
 }
 smallestOfThreeNumbers(2, 5, 3)
