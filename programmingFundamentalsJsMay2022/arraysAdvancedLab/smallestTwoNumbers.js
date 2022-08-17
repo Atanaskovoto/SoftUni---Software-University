33ec50b2600b899f630666a31ad9e7acb12d96ca
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-08-17 14:55:51 +0300

    Update smallestTwoNumbers.js
    Added another solution

--- programmingFundamentalsJsMay2022/arraysAdvancedLab/smallestTwoNumbers.js
+++ programmingFundamentalsJsMay2022/arraysAdvancedLab/smallestTwoNumbers.js
@@ -1,12 +1,21 @@
 function smallestTwoNumbers(array) {
     let copyOfArray = array.slice();
-    let smallestNumbers = [];
-    let index;
-    for (let i = 0; i < 2; i++) {
-        index = copyOfArray.indexOf(Math.min(...copyOfArray));
-        smallestNumbers.push(Math.min(...copyOfArray));
-        copyOfArray.splice(index, 1);
-    }
-    console.log(smallestNumbers.join(' '));
+
+    // Solution 1:
+
+    // let smallestNumbers = [];
+    // let index;
+    // for (let i = 0; i < 2; i++) {
+    //     index = copyOfArray.indexOf(Math.min(...copyOfArray));
+    //     smallestNumbers.push(Math.min(...copyOfArray));
+    //     copyOfArray.splice(index, 1);
+    // }
+    // console.log(smallestNumbers.join(' '));
+
+    // Solution 2:
+
+    copyOfArray.sort((a, b) => a - b);
+    let rezult = copyOfArray.slice(0, 2);
+    console.log(rezult.join(' '));
 }
 smallestTwoNumbers([30, 15, 50, 5]);
