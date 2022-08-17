999f0cd5acf0ad8c42a5d77f61377c171c2d7438
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-08-17 14:56:05 +0300

    Update processOddNumbers.js
    Added another solution

--- programmingFundamentalsJsMay2022/arraysAdvancedLab/processOddNumbers.js
+++ programmingFundamentalsJsMay2022/arraysAdvancedLab/processOddNumbers.js
@@ -1,11 +1,20 @@
 function processOddNumbers(input) {
-    let rezult = [];
-    let inputLength = input.length;
-    for (let index = inputLength - 1; index >= 0; index--) {
-        if (index % 2 !== 0) {
-            rezult.push(input[index] * 2);
-        }
-    }
-    console.log(rezult.join(' '));
+    // Solution 1:
+
+    // let rezult = [];
+    // let inputLength = input.length;
+    // for (let index = inputLength - 1; index >= 0; index--) {
+    //     if (index % 2 !== 0) {
+    //         rezult.push(input[index] * 2);
+    //     }
+    // }
+    // console.log(rezult.join(' '));
+
+    // Solution 2:
+
+    let filterEl = input.filter((x, i) => i % 2 !== 0);
+    let doubledEl = filterEl.map(x => x * 2);
+    let reverseEl = doubledEl.reverse();
+    console.log(reverseEl.join(' '));
 }
 processOddNumbers([3, 0, 10, 4, 7, 3]);
