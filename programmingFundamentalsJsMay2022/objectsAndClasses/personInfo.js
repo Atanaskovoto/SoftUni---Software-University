0eb5c1265435703cc29055500260ba5f4b7d8739
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-08-25 23:15:18 +0300

    Create personInfo.js
    Solved task

---
+++ programmingFundamentalsJsMay2022/objectsAndClasses/personInfo.js
@@ -0,0 +1,30 @@
+function personInfo(firstName, lastName, age) {
+    //    Solution 1:
+    // let info = {
+    //     firstName,
+    //     lastName,
+    //     age
+    // }
+    // return info;
+
+    //    Solution 2:
+    //     let info = {
+    //         firstName: firstName,
+    //         lastName: lastName,
+    //         age: age
+    //     }
+    // return info;
+
+
+    // Solution 3:
+    return {
+        firstName,
+        lastName,
+        age
+    }
+
+
+
+
+}
+personInfo("Peter", "Pan", "20")
