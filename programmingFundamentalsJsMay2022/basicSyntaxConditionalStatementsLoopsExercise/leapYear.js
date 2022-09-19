8edc637b1fac3c431ff89e90fc7554c75dce8500
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-09-19 21:59:12 +0300

    Update leapYear.js
    Another solution

--- programmingFundamentalsJsMay2022/basicSyntaxConditionalStatementsLoopsExercise/leapYear.js
+++ programmingFundamentalsJsMay2022/basicSyntaxConditionalStatementsLoopsExercise/leapYear.js
@@ -4,5 +4,15 @@ function leapYear(year) {
     } else {
         console.log('no');
     }
+
+    // Another solution:
+
+    // function solve(year) {
+    //     let isLeapYear = 'no';
+    //     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    //         isLeapYear = 'yes';
+    //     }
+    //     console.log(isLeapYear);
+    // }
 }
 leapYear(2000)
