a40d5a9c1b71ebf13c0377ef2576b6124be317c8
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-09-19 21:19:41 +0300

    Update ages.js
    Another solution

--- programmingFundamentalsJsMay2022/basicSyntaxConditionalStatementsLoopsExercise/ages.js
+++ programmingFundamentalsJsMay2022/basicSyntaxConditionalStatementsLoopsExercise/ages.js
@@ -12,5 +12,20 @@ function ages(age) {
     } else {
         console.log('out of bounds');
     }
+    // Another solution :
+    // let typeOfPerson = 'out of bounds';
+
+    // if (age >= 0 && age <= 2) {
+    //     typeOfPerson = 'baby';
+    // } else if (age >= 3 && age <= 13) {
+    //     typeOfPerson = 'child';
+    // } else if (age >= 14 && age <= 19) {
+    //     typeOfPerson = 'teenager';
+    // } else if (age >= 20 && age <= 65) {
+    //     typeOfPerson = 'adult';
+    // } else if (age >= 66) {
+    //     typeOfPerson = 'elder';
+    // }
+    // console.log(typeOfPerson);
 }
 ages(0);
