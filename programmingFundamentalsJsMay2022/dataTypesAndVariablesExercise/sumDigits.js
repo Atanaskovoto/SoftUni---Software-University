cfb5739144959d4657bf98374b294ae3a87cbf95
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-09-26 22:13:45 +0300

    Update sumDigits.js
    Another solution

--- programmingFundamentalsJsMay2022/dataTypesAndVariablesExercise/sumDigits.js
+++ programmingFundamentalsJsMay2022/dataTypesAndVariablesExercise/sumDigits.js
@@ -4,5 +4,14 @@ function sumDigits(number) {
     for (let i = 0; i < numToString.length; i++)
         sum += Number(numToString[i]);
     console.log(sum);
+
+    // Another solution:
+
+    // let numberToString = String(number);
+    // let sum = 0;
+    // for (let char of numberToString) {
+    //     sum += Number(char);
+    // }
+    // console.log(sum);
 }
 sumDigits(245678);
