b1c7b9fa8a730ba0444d59425e2f2901c1bfecea
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-09-26 22:13:29 +0300

    Update charsToString.js
    Another solution

--- programmingFundamentalsJsMay2022/dataTypesAndVariablesExercise/charsToString.js
+++ programmingFundamentalsJsMay2022/dataTypesAndVariablesExercise/charsToString.js
@@ -1,4 +1,9 @@
 function charsToString(char1, char2, char3) {
     console.log(`${char1}${char2}${char3}`);
+
+    // Another solution:
+
+    // let allCharacters = charOne + charTwo + charThree;
+    // console.log(allCharacters);
 }
 charsToString('a', 'b', 'c');
