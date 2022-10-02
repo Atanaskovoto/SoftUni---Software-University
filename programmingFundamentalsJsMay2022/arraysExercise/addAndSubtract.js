9bf0487588da60ee39805375bc421c29dbf8c8bc
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-02 17:40:49 +0300

    Update addAndSubtract.js
    Another solution

--- programmingFundamentalsJsMay2022/arraysExercise/addAndSubtract.js
+++ programmingFundamentalsJsMay2022/arraysExercise/addAndSubtract.js
@@ -14,5 +14,28 @@ function addAndSubtract(array) {
     console.log(newArray);
     console.log(arraySum);
     console.log(newArraySum);
+
+    // Another solution:
+
+    // let rezult = [];
+    //     let sumOfInput = 0;
+    //     let sumOfRezult = 0;
+    
+    //     for (let i = 0; i < input.length; i++) {
+    //         let currentNumber = input[i];
+    //         sumOfInput += input[i];
+    
+    //         if (currentNumber % 2 == 0) {
+    //             currentNumber += i;
+    //             rezult.push(currentNumber);
+    //         } else {
+    //             currentNumber -= i;
+    //             rezult.push(currentNumber);
+    //         }
+    //         sumOfRezult += currentNumber;
+    //     }
+    //     console.log(rezult);
+    //     console.log(sumOfInput);
+    //     console.log(sumOfRezult);
 }
 addAndSubtract([5, 15, 23, 56, 35])
