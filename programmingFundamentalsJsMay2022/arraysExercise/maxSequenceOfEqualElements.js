ecc1e2af06fa39ae6c28cee42a34e681fe5079bd
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-02 17:41:29 +0300

    Update maxSequenceOfEqualElements.js
    Another solution

--- programmingFundamentalsJsMay2022/arraysExercise/maxSequenceOfEqualElements.js
+++ programmingFundamentalsJsMay2022/arraysExercise/maxSequenceOfEqualElements.js
@@ -16,5 +16,27 @@ function maxSequenceOfEqualElements(array) {
         }
     }
     console.log(sequence.join(' '));
+
+    // Another solution:
+
+    // let maxSequence = [];
+
+    // for (let i = 0; i < input.length; i++) {
+    //     let currentSequence = [];
+    //     let currentElement = input[i];
+    //     currentSequence.push(currentElement);
+    //     for (let n = i + 1; n < input.length; n++) {
+    //         nextElement = input[n];
+    //         if (currentElement === nextElement) {
+    //             currentSequence.push(nextElement);
+    //         } else {
+    //             break;
+    //         }
+    //     }
+    //     if (maxSequence.length < currentSequence.length) {
+    //         maxSequence = currentSequence;
+    //     }
+    // }
+    // console.log(maxSequence.join(' '));
 }
 maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
