df22f3dc8bfec33a387ed0c1948c38da8f7e01e6
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-02 17:41:38 +0300

    Update mergeArrays.js
    Another solution

--- programmingFundamentalsJsMay2022/arraysExercise/mergeArrays.js
+++ programmingFundamentalsJsMay2022/arraysExercise/mergeArrays.js
@@ -8,6 +8,21 @@ function mergeArrays(arr1, arr2) {
         }
     }
     console.log(arr3.join(' - '));
+
+    // Another solution:
+
+    // let rezult = [];
+
+    // for (let i = 0; i < firstArray.length; i++) {
+    //     if (i % 2 == 0) {
+    //         let currentData = Number(firstArray[i]) + Number(secondArray[i]);
+    //         rezult.push(currentData);
+    //     } else {
+    //         let currentData = firstArray[i] + secondArray[i];
+    //         rezult.push(currentData);
+    //     }
+    // }
+    // console.log(rezult.join(' - '));
 }
 mergeArrays(['5', '15', '23', '56', '35'],
     ['17', '22', '87', '36', '11'])
