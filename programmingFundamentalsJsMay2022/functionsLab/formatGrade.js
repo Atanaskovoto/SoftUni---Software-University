44fadc701565bd3528d279da9f93faf0cca9f3f3
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-11 23:15:27 +0300

    Update formatGrade.js
    Another solution

--- programmingFundamentalsJsMay2022/functionsLab/formatGrade.js
+++ programmingFundamentalsJsMay2022/functionsLab/formatGrade.js
@@ -11,5 +11,23 @@ function formatGrade(grade) {
     } else {
         console.log(`Excellent (${grade.toFixed(2)})`);
     }
+
+    // Another solution:
+
+    // let description = '';
+
+    // if (grade < 3) {
+    //     description = 'Fail (2)';
+    // } else if (grade < 3.5) {
+    //     description = `Poor (${grade.toFixed(2)})`;
+    // } else if (grade < 4.5) {
+    //     description = `Good (${grade.toFixed(2)})`;
+    // } else if (grade < 5.5) {
+    //     description = `Very good (${grade.toFixed(2)})`;
+    // } else {
+    //     description = `Excellent (${grade.toFixed(2)})`;
+    // }
+    // console.log(description);
+
 }
 formatGrade(2.5);
