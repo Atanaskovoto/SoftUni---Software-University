f310fc5ad637322e24eff7fb6ea2ca4e88c79fec
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-11 23:15:07 +0300

    Update smallestOfThreeNumbers.js
    Another solution

--- programmingFundamentalsJsMay2022/functionsExercise/smallestOfThreeNumbers.js
+++ programmingFundamentalsJsMay2022/functionsExercise/smallestOfThreeNumbers.js
@@ -1,12 +1,31 @@
 function smallestOfThreeNumbers(numOne, numTwo, numThree) {
-    // let smallestNumber = numOne;
+    //Another solution:
+
+    //  let smallestNumber = numOne;
     // if (smallestNumber > numTwo) {
     //     smallestNumber = numTwo
     // }
     // if (smallestNumber > numThree) {
     //     smallestNumber = numThree
     // }
-    let smallestNumber = Math.min(numOne, numTwo, numThree)
-    console.log(smallestNumber);
+    // console.log(smallestNumber);
+    
+    // Another solution:
+
+    // let smallestNumber = Math.min(numOne, numTwo, numThree)
+    // console.log(smallestNumber);
+
+    let smallest = Number.MAX_SAFE_INTEGER;
+
+    let array = [];
+    array.push(numberOne, numberTwo, numberThree);
+
+    for (let i = 0; i < 3; i++) {
+        if (smallest > array[i]) {
+            smallest = array[i];
+        }
+    }
+
+    console.log(smallest);
 }
 smallestOfThreeNumbers(2, 5, 3)
