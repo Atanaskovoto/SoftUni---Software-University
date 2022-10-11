98b3abdd758a93a8bf79fb3dec6b3050519000cb
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-10-11 23:15:44 +0300

    Update repeatString.js
    Another solution

--- programmingFundamentalsJsMay2022/functionsLab/repeatString.js
+++ programmingFundamentalsJsMay2022/functionsLab/repeatString.js
@@ -8,3 +8,10 @@ function repeatString(string, repeat) {
 }
 let finalResult = repeatString('abc', 3)
 console.log(finalResult);
+
+// Another solution:
+
+// function solve(string, repeat) {
+//     return string.repeat(repeat);
+// }
+// console.log(solve('abc', 3));
