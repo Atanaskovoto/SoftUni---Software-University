fcee940c86befbbb87cb6bab89157d1374fa5055
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2022-11-04 18:24:58 +0200

    Update inventory.js
    Another solution

--- programmingFundamentalsJsMay2022/objectsAndClasses-Exercise/inventory.js
+++ programmingFundamentalsJsMay2022/objectsAndClasses-Exercise/inventory.js
@@ -8,12 +8,27 @@ function inventory(input) {
     allHeroes.forEach(hero => {
         hero[1] = Number(hero[1]);
     });
-    
+
     allHeroes.sort((a, b) => a[1] - b[1]);
 
     allHeroes.forEach(hero => {
         console.log(`Hero: ${hero[0]}\nlevel => ${hero[1]}\nitems => ${hero[2]}`);
     });
+
+    // Another solution:
+
+    // heroesInfo = [];
+
+    // for (const data of input) {
+    //     let [name, level, items] = data.split(' / ');
+    //     heroesInfo.push({ name, level, items });
+    // }
+
+    // heroesInfo.sort((a, b) => a.level - b.level);
+
+    // heroesInfo.forEach(hero => {
+    //     console.log(`Hero: ${hero.name}\nlevel => ${hero.level}\nitems => ${hero.items}`);
+    // });
 }
 inventory([
     'Isacc / 25 / Apple, GravityGun',
