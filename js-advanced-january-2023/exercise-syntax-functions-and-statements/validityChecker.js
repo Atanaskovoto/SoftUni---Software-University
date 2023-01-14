b41f2356f5e756844e5d4c84bbb3c51860742e06
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2023-01-14 21:59:43 +0200

    Create validityChecker.js
    Solved task

---
+++ js-advanced-january-2023/exercise-syntax-functions-and-statements/validityChecker.js
@@ -0,0 +1,19 @@
+function validityChecker(x1, y1, x2, y2) {
+
+    function isValid(x1, y1, x2, y2) {
+        const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if (Number.isInteger(distance)) {
+            return `{${x1}, ${y1}} to {${x2}, ${y2}} is valid`
+        }
+
+        return `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`
+    }
+
+    console.log(isValid(x1, y1, 0, 0));
+    console.log(isValid(x2, y2, 0, 0));
+    console.log(isValid(x1, y1, x2, y2));
+}
+
+validityChecker(3, 0, 0, 4);
+validityChecker(2, 1, 1, 1);
