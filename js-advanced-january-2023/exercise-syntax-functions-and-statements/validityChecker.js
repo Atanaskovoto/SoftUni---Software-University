f0f28731e535ba93bc1d6fdd4b3575f31e950db7
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2023-01-14 22:24:42 +0200

    Update validityChecker.js
    Task optimisation

--- js-advanced-january-2023/exercise-syntax-functions-and-statements/validityChecker.js
+++ js-advanced-january-2023/exercise-syntax-functions-and-statements/validityChecker.js
@@ -4,15 +4,15 @@ function validityChecker(x1, y1, x2, y2) {
         const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
         if (Number.isInteger(distance)) {
-            return `{${x1}, ${y1}} to {${x2}, ${y2}} is valid`
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+        } else {
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
         }
-
-        return `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`
     }
 
-    console.log(isValid(x1, y1, 0, 0));
-    console.log(isValid(x2, y2, 0, 0));
-    console.log(isValid(x1, y1, x2, y2));
+    isValid(x1, y1, 0, 0);
+    isValid(x2, y2, 0, 0);
+    isValid(x1, y1, x2, y2);
 }
 
 validityChecker(3, 0, 0, 4);
