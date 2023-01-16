88e99d46e378bb69fe84eb173e6f3fab95550205
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2023-01-16 23:58:15 +0200

    Create printAnArrayWithAGivenDelimiter.js
    Solved task

---
+++ js-advanced-january-2023/exercise-arrays-and-nested-arrays/printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,18 @@
+function printAnArrayWithAGivenDelimiter(input, delimiter) {
+console.log(input.join(delimiter));
+}
+
+printAnArrayWithAGivenDelimiter(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-');
+printAnArrayWithAGivenDelimiter(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+);
