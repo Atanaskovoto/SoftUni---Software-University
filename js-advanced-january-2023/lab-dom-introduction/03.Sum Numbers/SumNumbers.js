0613843800612d8330eec88ecd0b575451a8694d
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2023-01-22 11:33:14 +0200

    Update SumNumbers.js
    Another solution

--- js-advanced-january-2023/lab-dom-introduction/03.Sum Numbers/SumNumbers.js
+++ js-advanced-january-2023/lab-dom-introduction/03.Sum Numbers/SumNumbers.js
@@ -1,7 +1,21 @@
 function calc() {
-    const input1 = document.getElementById('num1');
-    const input2 = document.getElementById('num2');
-    const result = Number(input1.value) + Number(input2.value);
-    const sum = document.getElementById('sum');
-    sum.value = result;
+    // const input1 = document.getElementById('num1');
+    // const input2 = document.getElementById('num2');
+    // const result = Number(input1.value) + Number(input2.value);
+    // const sum = document.getElementById('sum');
+    // sum.value = result;
+
+    // Another solution:
+
+    // const input1 = document.getElementById('num1').value;
+    // const input2 = document.getElementById('num2').value;
+
+    // const result = Number(input1) + Number(input2);
+
+    // document.getElementById('sum').value = result;
+
+    // Another solution:
+
+    document.getElementById('sum').value = Number(document.getElementById('num1').value) + Number(document.getElementById('num2').value);
+
 }
