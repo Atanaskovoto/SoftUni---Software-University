3b4d9ed20c275daddfabf0c9dc2db99ede60e060
Atanaskovoto <103858849+Atanaskovoto@users.noreply.github.com>
2023-03-20 22:48:45 +0200

    Update app.js

--- js-applications-february-2023/exercise-client-side-rendering/01.List-Towns/app.js
+++ js-applications-february-2023/exercise-client-side-rendering/01.List-Towns/app.js
@@ -2,22 +2,19 @@ import { html, render } from "../../../node_modules/lit-html/lit-html.js";
 
 const form = document.querySelector('form');
 const root = document.getElementById('root');
-
 form.addEventListener('submit', addTowns);
 
+const townsTemplate = (data) => html`
+    <ul>
+        ${data.map(el => html`<li>${el}</li>`)}
+    </ul>
+     `;
 
 function addTowns(event) {
     event.preventDefault();
     const formData = new FormData(form);
-
     const towns = formData.get('towns').split(', ');
     form.reset();
-
-    const townsTemplate = () => html`
-    <ul>
-        ${towns.map(town => html`<li>${town}</li>`)}
-    </ul>
-     `;
-    render(townsTemplate(), root);
-
+    
+    render(townsTemplate(towns), root);
 }
